80e336ece92aa603ef8c5601dfa92373f3714f97
Gursimir <kgursimran648@gmail.com>
2020-10-14 00:01:56 +0530

    Class Templet

--- Class templet/class_templet.cpp
+++ Class templet/class_templet.cpp
@@ -7,26 +7,7 @@
 using namespace std;
 
 /*Write the class AddElements here*/
-template <class T>
-class AddElements {
-    T element;
-  public:
-    AddElements (T arg) {element=arg;}
-    T add (T x) {return x+element;}
-};
 
-// class template specialization:
-template <>
-class AddElements <string> {
-    string element;
-  public:
-    AddElements (string arg) {element=arg;}
-    string concatenate (string arg)
-    {
-      string s = element+arg;
-      return s;
-    }
-};
 int main () {
     #ifndef ONLINE_JUDGE
         freopen("input.txt","r",stdin);
